3e5e895efafe11582be290e56367d11ba486757b
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2023-10-23 09:27:06 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+// www.codewars.com
+// KATA 1 7kyu
+// https://www.codewars.com/kata/5acbc3b3481ebb23a400007d/train/javascript
+
+// H hearts S Spades C clubs D Diamonds
+const isFlush = (cards) => {
+  let suit = cards[0].slice([-1]);
+  let result;
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].slice(-1) !== suit) {
+      result = false;
+      return result;
+    } else {
+      result = true;
+    }
+  }
+  return result;
+};
+
+console.log(isFlush(["AS", "3S", "9S", "KS", "4S"]));
+console.log(isFlush(["AD", "4S", "7H", "KS", "10S"]));
+
+// KATA 2 7kyu
+const transposeTwoStrings = (arr) => {
+  let newArr = [];
+  let maxLen = Math.max(arr[0].length, arr[1].length);
+  for (let i = 0; i < maxLen; i++) {
+    newArr.push(`${arr[0][i] || " "} ${arr[1][i] || " "}`);
+  }
+  return newArr.join("\n");
+};
+
+console.log(transposeTwoStrings(["Hello", "World"]));
+
+// KATA 3 8kyu
+const stringToArray = (string) => string.split(" ");
+
+console.log(stringToArray("I love strings and arrays."));
